ba40bdbcdbe18e6b7a45cb8d3eb9f564c7644801
RutaTang <rutatangpersonal@gmail.com>
2022-12-30 22:59:10 +0800

    refactoring new,open,delete note at current line for current buffer, works well

--- lua/quicknote/core/new.lua
+++ lua/quicknote/core/new.lua
@@ -3,48 +3,48 @@ local path = require("plenary.path")
 local utils_fs = require("quicknote.utils.fs")
 local sha = require("quicknote.utils.sha")
 
+-- Get the path to the notes directory corresponding to the current buffer
+-- Note: one buffer can have multiple notes; the name of the dir is the hash of the buffer file name
+local getNoteDirPathForCurrentBuffer = function()
+    -- get current buffer file path
+    local currentBufferPath = vim.api.nvim_buf_get_name(0)
+
+    -- get note dir path
+    local dataPath = utils_fs.GetDataPath()
+    local noteDirName = sha.sha1(currentBufferPath) -- hash current buffer path
+    local noteDirPath = path:new(dataPath, noteDirName).filename
+
+    return noteDirPath
+end
+
 -- Export
 local M = {}
 
 -- Create a new note at current cursor line for current buffer
 local NewNoteAtCurrentLineAsync = function()
-    -- get current
+    -- get current cursor line
     local line = vim.api.nvim_win_get_cursor(0)[1]
-    -- get current buffer file path
-    local currentBufferPath = vim.api.nvim_buf_get_name(0)
 
-    -- get note dir path
-    local dataPath = utils_fs.GetDataPath()
-    local noteDirName = sha.sha1(currentBufferPath) -- hash current buffer path
-    local noteDirPath = path:new(dataPath, noteDirName).filename
+    -- get note file path
+    local noteDirPath = getNoteDirPathForCurrentBuffer()
 
-    -- create note dir
+    -- create note dir (if not exist)
     utils_fs.MKDirAsync(noteDirPath)
 
     -- create note file (if not exist)
     local noteFilePath = path:new(noteDirPath, line .. ".md").filename
     utils_fs.CreateFileAsync(noteFilePath)
-
-    return noteFilePath
 end
 M.NewNoteAtCurrentLine = function()
     async.run(NewNoteAtCurrentLineAsync, function() end)
 end
 
+-- Open an already existed note at current cursor line for current buffer
 local OpenNoteAtCurrentLine = function()
-    -- get current
     local line = vim.api.nvim_win_get_cursor(0)[1]
-    -- get current buffer file path
-    local currentBufferPath = vim.api.nvim_buf_get_name(0)
-
-    -- get note dir path
-    local dataPath = utils_fs.GetDataPath()
-    local noteDirName = sha.sha1(currentBufferPath) -- hash current buffer path
-    local noteDirPath = path:new(dataPath, noteDirName).filename
-
+    local noteDirPath = getNoteDirPathForCurrentBuffer()
     -- get note file path
     local noteFilePath = path:new(noteDirPath, line .. ".md").filename
-
     -- check if note file exist
     vim.loop.fs_stat(noteFilePath, function(err, _)
         if err then
@@ -60,17 +60,10 @@ local OpenNoteAtCurrentLine = function()
 end
 M.OpenNoteAtCurrentLine = OpenNoteAtCurrentLine
 
+-- Delete an already existed note at current cursor line for current buffer
 local DeleteNoteAtCurrentLine = function()
-    -- get current
     local line = vim.api.nvim_win_get_cursor(0)[1]
-    -- get current buffer file path
-    local currentBufferPath = vim.api.nvim_buf_get_name(0)
-
-    -- get note dir path
-    local dataPath = utils_fs.GetDataPath()
-    local noteDirName = sha.sha1(currentBufferPath) -- hash current buffer path
-    local noteDirPath = path:new(dataPath, noteDirName).filename
-
+    local noteDirPath = getNoteDirPathForCurrentBuffer()
     -- get note file path
     local noteFilePath = path:new(noteDirPath, line .. ".md").filename
 
@@ -80,7 +73,7 @@ local DeleteNoteAtCurrentLine = function()
             print("Note not found.")
         else
             -- delete note file
-            local success,err = os.remove(noteFilePath)
+            local success, err = os.remove(noteFilePath)
             if success then
                 print("Note deleted.")
             else
