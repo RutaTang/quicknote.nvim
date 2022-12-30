8441cc8b044c50be0525cf590e54b2bcb9c6e95b
RutaTang <rutatangpersonal@gmail.com>
2022-12-30 13:18:20 +0800

    init

--- lua/quicknote/core/new.lua
+++ lua/quicknote/core/new.lua
@@ -10,28 +10,26 @@ local function mkdir(path)
         end
     end
 end
-async.run(function ()
-   mkdir(dataPath)
-end)
 
 local M = {}
 
 local newNoteAtCurrentLine = function()
-    -- get current
-    local line = vim.api.nvim_win_get_cursor(0)[1]
-    -- get current buffer file path
-    local currentBufferPath = vim.api.nvim_buf_get_name(0)
-    -- note dir path
-    local noteDirPath = dataPath .. "/" .. "[" .. currentBufferPath .. "]" .. ".quicknote"
-    -- check quick note dir for this buffer existance
-    local err, stat = async.uv.fs_stat(noteDirPath)
-    if err or stat == nil then
-        -- create quick note dir for this buffer
-        local err, success = async.uv.fs_mkdir(noteDirPath, 511)
-        if err or not success then
-            error("Error: " .. err)
-        end
-    end
+    mkdir(dataPath)
+    -- -- get current
+    -- local line = vim.api.nvim_win_get_cursor(0)[1]
+    -- -- get current buffer file path
+    -- local currentBufferPath = vim.api.nvim_buf_get_name(0)
+    -- -- note dir path
+    -- local noteDirPath = dataPath .. "/" .. "[" .. currentBufferPath .. "]" .. ".quicknote"
+    -- -- check quick note dir for this buffer existance
+    -- local err, stat = async.uv.fs_stat(noteDirPath)
+    -- if err or stat == nil then
+    --     -- create quick note dir for this buffer
+    --     local err, success = async.uv.fs_mkdir(noteDirPath, 511)
+    --     if err or not success then
+    --         error("Error: " .. err)
+    --     end
+    -- end
 end
 
 M.NewNoteAtCurrentLine = function()
