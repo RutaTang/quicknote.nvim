d7efc1d3c02ec88dc19e842b3b65e83f2d04dd54
RutaTang <rutatangpersonal@gmail.com>
2022-12-30 14:29:55 +0800

    init

--- lua/quicknote/init.lua
+++ lua/quicknote/init.lua
@@ -1,27 +1,27 @@
-local utils = require('quicknote.utils')
+local utils_fs = require('quicknote.utils.fs')
 local new = require('quicknote.core.new')
 
 -- init data folder for quick note
-utils.MKDir(utils.GetDataPath())
+utils_fs.MKDir(utils_fs.GetDataPath())
 
 -- Define API
 return {
-    NewNoteAtCWD = utils.todo("NewNoteAtCWD"),
-    NewNoteAtLine = utils.todo("NewNoteAtLine"),
+    NewNoteAtCWD = utils_fs.todo("NewNoteAtCWD"),
+    NewNoteAtLine = utils_fs.todo("NewNoteAtLine"),
     NewNoteAtCurrentLine = new.NewNoteAtCurrentLine,
-    NewNoteAtGlobal = utils.todo("NewNoteAtGlobal"),
+    NewNoteAtGlobal = utils_fs.todo("NewNoteAtGlobal"),
 
-    GetNoteAtLine = utils.todo("GetNoetAtLine"),
-    GetNoteAtCurrentLine = utils.todo("GetNoteAtCurrentLine"),
-    GetNoteAtGlobal = utils.todo("GetNoteAtGlobal"),
-    GetNoteAtCWD = utils.todo("GetNoteAtCWD"),
+    GetNoteAtLine = utils_fs.todo("GetNoetAtLine"),
+    GetNoteAtCurrentLine = utils_fs.todo("GetNoteAtCurrentLine"),
+    GetNoteAtGlobal = utils_fs.todo("GetNoteAtGlobal"),
+    GetNoteAtCWD = utils_fs.todo("GetNoteAtCWD"),
 
-    DeleteNoteAtLine = utils.todo("DeleteNoteAtLine"),
-    DeleteNoteAtCurrentLine = utils.todo("DeleteNoteAtCurrentLine"),
-    DeleteNoteAtGlobal = utils.todo("DeleteNoteAtGlobal"),
-    DeleteNoteAtCWD = utils.todo("DeleteNoteAtCWD"),
+    DeleteNoteAtLine = utils_fs.todo("DeleteNoteAtLine"),
+    DeleteNoteAtCurrentLine = utils_fs.todo("DeleteNoteAtCurrentLine"),
+    DeleteNoteAtGlobal = utils_fs.todo("DeleteNoteAtGlobal"),
+    DeleteNoteAtCWD = utils_fs.todo("DeleteNoteAtCWD"),
 
-    ShowNoteSign = utils.todo("ShowNoteSign"),
-    HideNoteSign = utils.todo("HideNoteSign"),
-    ToggleNoteSign = utils.todo("ToggleNoteSign"),
+    ShowNoteSign = utils_fs.todo("ShowNoteSign"),
+    HideNoteSign = utils_fs.todo("HideNoteSign"),
+    ToggleNoteSign = utils_fs.todo("ToggleNoteSign"),
 }
