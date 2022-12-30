cfcf78cf242e124f9f2d6970a1333f52232ddb7c
RutaTang <rutatangpersonal@gmail.com>
2022-12-30 13:15:22 +0800

    init

--- lua/quicknote/core/new.lua
+++ lua/quicknote/core/new.lua
@@ -1,5 +1,18 @@
-local async = require('plenary.async')
+local async = require("plenary.async")
 local dataPath = vim.fn.stdpath("data") .. "/quicknote"
+
+local function mkdir(path)
+    local err, stat = async.uv.fs_stat(path)
+    if err or stat == nil then
+        local err, success = async.uv.fs_mkdir(path, 511)
+        if err or not success then
+            error("Error: " .. err)
+        end
+    end
+end
+async.run(function ()
+   mkdir(dataPath)
+end)
 
 local M = {}
 
@@ -11,18 +24,18 @@ local newNoteAtCurrentLine = function()
     -- note dir path
     local noteDirPath = dataPath .. "/" .. "[" .. currentBufferPath .. "]" .. ".quicknote"
     -- check quick note dir for this buffer existance
-    local err,stat = async.uv.fs_stat(noteDirPath)
+    local err, stat = async.uv.fs_stat(noteDirPath)
     if err or stat == nil then
         -- create quick note dir for this buffer
-        local err,success = async.uv.fs_mkdir(noteDirPath, 511)
+        local err, success = async.uv.fs_mkdir(noteDirPath, 511)
         if err or not success then
             error("Error: " .. err)
         end
     end
 end
 
-M.NewNoteAtCurrentLine = function ()
-   async.run(newNoteAtCurrentLine)
+M.NewNoteAtCurrentLine = function()
+    async.run(newNoteAtCurrentLine)
 end
 
 return M
