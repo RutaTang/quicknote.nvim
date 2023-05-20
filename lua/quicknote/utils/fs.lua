a0d808d34a7f897ae0bb8ae419609eb8001c6121
RutaTang <rutatangpersonal@gmail.com>
2023-05-21 00:24:48 +0800

    Fix: make dir and file safter

--- lua/quicknote/utils/fs.lua
+++ lua/quicknote/utils/fs.lua
@@ -9,7 +9,7 @@ local M = {}
 M.MKDirAsync = function(path)
     local err, stat = async.uv.fs_stat(path)
     if err or stat == nil then
-        local err, success = async.uv.fs_mkdir(path, tonumber("0777", 8)) -- 711
+        local err, success = async.uv.fs_mkdir(path, tonumber("666", 8))
         if err or not success then
             error("Error: " .. err)
         end
@@ -20,13 +20,12 @@ M.CreateFileAsync = function(path)
     local err, stat = async.uv.fs_stat(path)
     if err or stat == nil then
         -- create note file for current line
-        local err, fd = async.uv.fs_open(path, "w", tonumber("0777", 8)) -- 666
+        local err, fd = async.uv.fs_open(path, "w", tonumber("666", 8))
         if err or fd == nil then
             error("Error: " .. err)
         end
         async.uv.fs_close(fd)
     end
 end
-
 
 return M
